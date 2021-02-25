94912ab1692cc62ac6ebd052b5c25f76be844bcf
Roman BAD <57302328+romanoz33@users.noreply.github.com>
2021-02-25 12:09:15 +0300

    Changes

--- src/components/Item.js
+++ src/components/Item.js
@@ -189,8 +189,8 @@ const Item = ({
 			onClick={e => openGalleryItem(e)}
 			max-width='100%'
 			max-height='100%'
-			width={getItemSize()}
-			height={getItemSize()}
+			width={getItemSize}
+			height={getItemSize}
 			min-width={imagesAutoResizeProp ? '100%' : 'auto'}
 			min-height={imagesAutoResizeProp ? '100%' : 'auto'}
 			object-fit={imagesAutoResizeProp ? 'cover' : objectFitPreview}
